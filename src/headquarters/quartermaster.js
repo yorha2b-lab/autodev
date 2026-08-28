de9d2d222aca01a932e8da99125ed3654ab62c76
yorha2b-lab <266277844+yorha2b-lab@users.noreply.github.com>
2026-08-28 12:51:55 +0800

    fix:调整军需官逻辑

--- src/headquarters/quartermaster.js
+++ src/headquarters/quartermaster.js
@@ -4,8 +4,11 @@ const chalk = require('chalk')
 module.exports = ({ dialog }) => ({
     dirs: dir => fs.mkdirSync(dir, { recursive: true }),
     files: ({ from, to, exist, success }) => {
-        if (fs.existsSync(to) && !!exist) {
-            return console.log(chalk.yellow(dialog.bunker[exist]))
+        if (fs.existsSync(to)) {
+            if (!!exist) {
+                return console.log(chalk.yellow(dialog.bunker[exist]))
+            }
+            return
         }
         fs.cpSync(from, to, { recursive: true })
         if (!!success) {
